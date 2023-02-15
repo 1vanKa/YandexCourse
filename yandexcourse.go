7f3fdbddc42854e9cfd15ed6820a052ef53c901f
1vanKa <ivan.karpovich@gmail.com>
2023-02-15 10:31:18 +0300

    Multiplication with type convertion

--- yandexcourse.go
+++ yandexcourse.go
@@ -1,42 +1,25 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
+	"fmt"
 	"strings"
 )
 
-type limitReader struct {
-	reader io.Reader
-	left   int
-}
-
-func (l *limitReader) Read(p []byte) (n int, err error) {
-	if l.left == 0 {
-		return 0, io.EOF
-	}
-	if l.left < len(p) {
-		p = p[:l.left]
-	}
-	n, err = l.reader.Read(p)
-	l.left -= n
-	return n, err
-}
-
-func LimitReader(r io.Reader, n int) io.Reader {
-	return &limitReader{
-		reader: r,
-		left:   n,
+func Mul(a interface{}, b int) interface{} {
+	switch a2 := a.(type) {
+	case int:
+		return a2 * b
+	case string:
+		return strings.Repeat(a2, b)
+	case fmt.Stringer:
+		return strings.Repeat(a2.String(), b)
+	default:
+		panic("unrecognized type; only string, Stringer and int are accepted")
 	}
 }
 
 func main() {
-	r := strings.NewReader("some io.Reader stream to be read\n")
-	lr := LimitReader(r, 4)
-
-	_, err := io.Copy(os.Stdout, lr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(Mul(1, 5))
+	fmt.Println(Mul("12", 5))
+	fmt.Println(Mul(nil, 5))
 }
