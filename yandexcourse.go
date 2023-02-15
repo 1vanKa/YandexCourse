422bfbfd3fc47631c8f1ffbb7219a764a09c1df2
1vanKa <ivan.karpovich@gmail.com>
2023-02-15 09:58:26 +0300

    LimitReader

--- yandexcourse.go
+++ yandexcourse.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"yandexcourse/hashbyte"
-	"yandexcourse/randbyte"
+	"io"
+	"log"
+	"os"
+	"strings"
 )
 
-func main() {
+type limitReader struct {
+	reader io.Reader
+	left   int
+}
 
-	// создаём генератор случайных чисел
-	generator := randbyte.New(time.Now().UnixNano()) // в качестве затравки передаём ему текущее время, и при каждом запуске оно будет разным.
-
-	buf := make([]byte, 16)
-
-	for i := 0; i < 5; i++ {
-		n, _ := generator.Read(buf) // единственный доступный метод, но он нам и нужен.
-		fmt.Printf("Generate bytes: %v size(%d)\n", buf, n)
+func (l *limitReader) Read(p []byte) (n int, err error) {
+	if l.left == 0 {
+		return 0, io.EOF
 	}
+	if l.left < len(p) {
+		p = p[:l.left]
+	}
+	n, err = l.reader.Read(p)
+	l.left -= n
+	return n, err
+}
 
-	hasher := hashbyte.New(0)
-	hasher.Write(buf)
-	fmt.Printf("Hash: %v \n", hasher.Hash())
+func LimitReader(r io.Reader, n int) io.Reader {
+	return &limitReader{
+		reader: r,
+		left:   n,
+	}
+}
+
+func main() {
+	r := strings.NewReader("some io.Reader stream to be read\n")
+	lr := LimitReader(r, 4)
+
+	_, err := io.Copy(os.Stdout, lr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
