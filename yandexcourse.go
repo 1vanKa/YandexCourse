e6e34d5bbd0f43b20ae508e6ac40f580bf8d7859
1van_ka <ivan.karpovich@gmail.com>
2023-02-22 13:06:24 +0300

    Errors

--- yandexcourse.go
+++ yandexcourse.go
@@ -1,120 +1,76 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
-	"log"
-	"reflect"
+	"os"
+	"regexp"
+	"strings"
 )
 
-type Spell interface {
-	// название заклинания
-	Name() string
-	// характеристика, на которую воздействует
-	Char() string
-	// количественное значение
-	Value() int
+// 1) вставьте определение типа для []error
+// 2) определите метод Error для вашего типа, который будет выводить
+//    все ошибки слайса
+// 3) реализуйте функцию MyCheck
+//
+// ...
+
+type errorslice struct {
+	errs []error
 }
 
-// CastReceiver — если объект удовлетворяет этом интерфейсу, то заклинание применяется через него
-type CastReceiver interface {
-	ReceiveSpell(s Spell)
-}
-
-func CastToAll(spell Spell, objects []interface{}) {
-	for _, obj := range objects {
-		CastTo(spell, obj)
+func (err errorslice) Error() string {
+	sb := strings.Builder{}
+	for _, e := range err.errs {
+		sb.WriteString(e.Error())
+		sb.WriteString("; ")
 	}
+	return strings.TrimRight(sb.String(), "; ")
 }
 
-func CastTo(spell Spell, object interface{}) {
-	// реализуйте эту функцию.
-	if receiver_obj, ok := object.(CastReceiver); ok {
-		receiver_obj.ReceiveSpell(spell)
-		log.Printf("Spell %v casted to %#v", spell, object)
-		return
+var isNumeric func(string) bool = regexp.MustCompile(`\d`).MatchString
+
+var (
+	ErrFoundNumbers = errors.New("found numbers")
+	ErrLineTooLong  = errors.New("line is too long")
+	ErrNoTwoSpaces  = errors.New("no two spaces")
+)
+
+func MyCheck(input string) error {
+	err := errorslice{}
+	if isNumeric(input) {
+		err.errs = append(err.errs, ErrFoundNumbers)
 	}
-	value_obj := reflect.ValueOf(object)
-	if value_obj.Kind() != reflect.Ptr || value_obj.Elem().Kind() != reflect.Struct {
-		return
+	if len(input) >= 20 {
+		err.errs = append(err.errs, ErrLineTooLong)
 	}
-
-	if field := value_obj.Elem().FieldByName(spell.Char()); field.IsValid() {
-		if field.CanSet() {
-			field.SetInt(int64(spell.Value()) + field.Int())
-			log.Printf("Spell %v casted to %#v", spell, object)
-		}
+	if strings.Count(input, " ") != 2 {
+		err.errs = append(err.errs, ErrNoTwoSpaces)
 	}
-}
-
-type spell struct {
-	name string
-	char string
-	val  int
-}
-
-func newSpell(name string, char string, val int) Spell {
-	return &spell{name: name, char: char, val: val}
-}
-
-func (s spell) Name() string {
-	return s.name
-}
-
-func (s spell) Char() string {
-	return s.char
-}
-
-func (s spell) Value() int {
-	return s.val
-}
-
-type Player struct {
-	name   string
-	health int
-}
-
-func (p *Player) ReceiveSpell(s Spell) {
-	if s.Char() == "Health" {
-		p.health += s.Value()
+	if len(err.errs) > 0 {
+		return err
 	}
-}
-
-type Zombie struct {
-	Health int
-}
-
-type Daemon struct {
-	Health int
-}
-
-type Orc struct {
-	Health int
-}
-
-type Wall struct {
-	Durability int
+	return nil
 }
 
 func main() {
-
-	player := &Player{
-		name:   "Player_1",
-		health: 100,
+	for {
+		fmt.Printf("Укажите строку (q для выхода): ")
+		reader := bufio.NewReader(os.Stdin)
+		ret, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		ret = strings.TrimRight(ret, "\n")
+		if ret == `q` {
+			break
+		}
+		if err = MyCheck(ret); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(`Строка прошла проверку`)
+		}
 	}
-
-	enemies := []interface{}{
-		&Zombie{Health: 1000},
-		&Zombie{Health: 1000},
-		&Orc{Health: 500},
-		&Orc{Health: 500},
-		&Orc{Health: 500},
-		&Daemon{Health: 1000},
-		&Daemon{Health: 1000},
-		&Wall{Durability: 100},
-	}
-
-	CastToAll(newSpell("fire", "Health", -50), append(enemies, player))
-	CastToAll(newSpell("heal", "Health", 190), append(enemies, player))
-
-	fmt.Println(player)
 }
